401f7fc5ec2b242b92cf1a75008c42a85f45454a
george7923 <120937745+george7923@users.noreply.github.com>
2024-07-02 09:51:39 +0300

    Add files via upload

---
+++ DTOs/ReviewDto.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace PRACTICA_OFICIAL.DTOs
+{
+    public class ReviewDto
+    {
+        [Range(1.0, 5.0)]
+        public double NumarStele { get; set; } 
+        public string? Parere { get; set; }
+        public string NumeRestaurant { get; set; }
+        public string Username { get; set; }
+    }
+}
