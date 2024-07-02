2a38de379083fafe44f89217dcc6517de55a8fd0
george7923 <120937745+george7923@users.noreply.github.com>
2024-07-02 09:53:04 +0300

    Update ComandaDto.cs

--- DTOs/ComandaDto.cs
+++ DTOs/ComandaDto.cs
@@ -1,10 +1,26 @@
-﻿namespace PRACTICA_OFICIAL.DTOs
+using System;
+using System.Collections.Generic;
+using System.Text.Json;
+using System.Text.Json.Serialization;
+
+namespace PRACTICA_OFICIAL.DTOs
 {
     public class ComandaDto
     {
         public string Username { get; set; }
-        public string NumeRestaurant { get; set; } // NumeleRestaurantului
+        public string NumeRestaurant { get; set; }
         public List<ProdusDto> Produse { get; set; }
-        public double? Tips { get; set; } // Added Tips
+        public double? Tips { get; set; }
+        public int? Cantitate { get; set; }
+        public bool Platit { get; set; }
+        public bool Livrat { get; set; }
+
+        [JsonConverter(typeof(DateTimeConverter))]
+        public DateTime DataComenzii { get; set; }
+
+        [JsonConverter(typeof(DateTimeConverter))]
+        public DateTime DataLivrare { get; set; }
     }
+
+
 }
