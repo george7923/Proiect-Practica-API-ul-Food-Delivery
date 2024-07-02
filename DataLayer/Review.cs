f0e4dbc7f5354092e6723a01a8ee2bab27069a16
george7923 <120937745+george7923@users.noreply.github.com>
2024-07-02 09:50:40 +0300

    Add files via upload

---
+++ DataLayer/Review.cs
@@ -0,0 +1,25 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace PRACTICA_OFICIAL.DataLayer
+{
+    [Table("Reviewuri")]
+    public class Review
+    {
+        [Key]
+        public int IdReview { get; set; }
+
+        [Range(1.0, 5.0)]
+        public double NumarStele { get; set; } 
+
+        public string? Parere { get; set; }
+
+        [ForeignKey("Restaurant")]
+        public int IdRestaurant { get; set; }
+        public Restaurant Restaurant { get; set; }
+
+        [ForeignKey("Cont")]
+        public int IdCont { get; set; }
+        public Cont Cont { get; set; }
+    }
+}
