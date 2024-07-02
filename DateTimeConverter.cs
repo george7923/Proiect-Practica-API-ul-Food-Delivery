893cda293ed7ff671e4b2a59debfc494faad344a
george7923 <120937745+george7923@users.noreply.github.com>
2024-07-02 09:54:37 +0300

    Add files via upload

---
+++ DateTimeConverter.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Text.Json;
+using System.Text.Json.Serialization;
+
+namespace PRACTICA_OFICIAL
+{
+    public class DateTimeConverter : JsonConverter<DateTime>
+    {
+        private readonly string _format = "yyyy-MM-dd HH:mm:ss";
+
+        public override DateTime Read(ref Utf8JsonReader reader, Type typeToConvert, JsonSerializerOptions options)
+        {
+            if (reader.TokenType == JsonTokenType.String)
+            {
+                if (DateTime.TryParseExact(reader.GetString(), _format, null, System.Globalization.DateTimeStyles.None, out DateTime date))
+                {
+                    return date;
+                }
+                return DateTime.Parse(reader.GetString());
+            }
+            return reader.GetDateTime();
+        }
+
+        public override void Write(Utf8JsonWriter writer, DateTime value, JsonSerializerOptions options)
+        {
+            writer.WriteStringValue(value.ToString(_format));
+        }
+    }
+}
